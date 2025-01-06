d9a3e365e4d8525ef0b43c79694332000ced8ba4
Faril Mammadov <memmedovfaril@gmail.com>
2025-01-07 00:17:49 +0400

    chore : Fonts added to constant file

---
+++ src/constants/Fonts.js
@@ -0,0 +1,13 @@
+const Fonts = {
+  '100Thin': 'AlbertSans_100Thin',
+  '200ExtraLight': 'AlbertSans_200ExtraLight',
+  '300Light': 'AlbertSans_300Light',
+  '400Regular': 'AlbertSans_400Regular',
+  '500Medium': 'AlbertSans_500Medium',
+  '600SemiBold': 'AlbertSans_600SemiBold',
+  '700Bold': 'AlbertSans_700Bold',
+  '800ExtraBold': 'AlbertSans_800ExtraBold',
+  '900Black': 'AlbertSans_900Black',
+};
+
+export default Fonts;
