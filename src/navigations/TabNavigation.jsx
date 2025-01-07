217aa41c6565c24c8944c530a20e3a03e4a06167
Faril Mammadov <memmedovfaril@gmail.com>
2025-01-08 00:12:08 +0400

    feat: tab navigation styling added

--- src/navigations/TabNavigation.jsx
+++ src/navigations/TabNavigation.jsx
@@ -1,22 +1,47 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import { View, Text } from 'react-native'
-import React from 'react'
 import HomeScreen from "@/screens/HomeScreen";
 import BmiTrackScreen from "@/screens/BmiTrackScreen";
 import WaterTrackScreen from "@/screens/WaterTrackScreen";
 import HealthyFoodsScreen from "@/screens/HealthyFoodsScreen";
+import Fonts from "@/constants/Fonts";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useTheme } from "@react-navigation/native";
 
 const TabNavigation = () => {
   const { Navigator, Screen, Group } = createBottomTabNavigator();
+  const { bottom } = useSafeAreaInsets();
+  const { colors } = useTheme();
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerTitleStyle: {
+          fontFamily: Fonts["600SemiBold"],
+          fontSize: 14,
+        },
+        tabBarStyle: {
+          marginBottom: bottom,
+          marginHorizontal: 20,
+          borderRadius: 24,
+          elevation: 0,
+          borderColor: colors.border,
+          borderWidth: 1,
+          borderTopWidth: 1,
+        },
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        },
+      }}
+    >
       <Screen name="Home" component={HomeScreen} />
       <Screen name="Bmi" component={BmiTrackScreen} />
       <Screen name="Water" component={WaterTrackScreen} />
       <Screen name="Foods" component={HealthyFoodsScreen} />
     </Navigator>
-  )
-}
+  );
+};
 
-export default TabNavigation
+export default TabNavigation;
