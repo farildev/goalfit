ef84ada5f036b0f2afeab3fdcdbf9d10decd8d02
Faril Mammadov <memmedovfaril@gmail.com>
2025-01-09 01:08:12 +0400

    feat : bmi result screen added

--- src/navigations/StackNavigation.jsx
+++ src/navigations/StackNavigation.jsx
@@ -1,14 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { View, Text } from 'react-native'
 import React from 'react'
 import TabNavigation from "./TabNavigation";
+import BmiResultScreen from "@/screens/BmiResultScreen";
 
 const StackNavigation = () => {
   const { Navigator, Screen, Group } = createNativeStackNavigator();
   return (
     <Navigator>
       <Screen options={{headerShown : false}} name="Tabs" component={TabNavigation} />
+      <Screen options={{headerShown : false}} name="BmiResult" component={BmiResultScreen} />
     </Navigator>
   )
 }
