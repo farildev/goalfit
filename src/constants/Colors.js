f745c919b82e3cdb20defc514519d479cdd4e934
Faril Mammadov <memmedovfaril@gmail.com>
2025-01-24 13:08:41 +0400

    chore : colors added to file

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -13,13 +13,17 @@ export const Colors = {
     text: '#151515',
     border: '#ededed',
     tabBarBg : "#151515",
+    button : "#000",
+    placeholder : "#cecece"
   },
 
   dark: {
     background: '#151515',
     cardItem: '#1d1d1d',
     text: '#fff',
-    border: '#ededed',
-    tabBarBg : "#fff"
+    border: '#282828',
+    tabBarBg : "#1d1d1d",
+    button : "#000",
+    placeholder : '#cdcdcd'
   }
 }
