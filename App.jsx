5dac7d3a803e70bfdfce4da12ccc6b5d8ea42736
Faril Mammadov <memmedovfaril@gmail.com>
2025-01-24 13:04:46 +0400

    feat : colors scheme added for statusbar

--- App.jsx
+++ App.jsx
@@ -3,13 +3,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import RootNavigation from '@/navigations/RootNavigation'
 import FontLoader from '@/hocs/FontLoader'
-
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 const App = () => {
+  const colorScheme = useColorScheme();
   return (
     <GestureHandlerRootView style={{flex : 1}}>
       <SafeAreaProvider>
         <FontLoader>
           <RootNavigation />
+          <StatusBar style={colorScheme === 'dark' ? 'dark' : 'light'} />
         </FontLoader>
       </SafeAreaProvider>
     </GestureHandlerRootView>
