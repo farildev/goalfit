ec7053913e2fd9ea3aa8ed83e125e30bf793d6ec
Faril Mammadov <memmedovfaril@gmail.com>
2025-04-10 01:50:39 +0400

    feat: HomeScreen updated

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,122 @@
 import { View, Text } from 'react-native'
-import { useTheme } from '@react-navigation/native'
+import { useTheme } from '@react-navigation/native';
+import { PieChart } from "react-native-gifted-charts";
+
 const HomeScreen = () => {
   const { colors } = useTheme();
+
+  const pieData = [
+    {
+      value: 47,
+      color: '#009FFF',
+      gradientCenterColor: '#006DFF',
+      focused: true,
+    },
+    { value: 40, color: '#93FCF8', gradientCenterColor: '#3BE9DE' },
+    { value: 16, color: '#BDB2FA', gradientCenterColor: '#8F80F3' },
+    { value: 3, color: '#FFA5BA', gradientCenterColor: '#FF7F97' },
+  ];
+
+  const renderDot = color => {
+    return (
+      <View
+        style={{
+          height: 10,
+          width: 10,
+          borderRadius: 5,
+          backgroundColor: color,
+          marginRight: 10,
+        }}
+      />
+    );
+  };
+
+  const renderLegendComponent = () => {
+    return (
+      <>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: 10,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              width: 120,
+              marginRight: 20,
+            }}>
+            {renderDot('#006DFF')}
+            <Text style={{ color: 'white' }}>Excellent: 47%</Text>
+          </View>
+          <View
+            style={{ flexDirection: 'row', alignItems: 'center', width: 120 }}>
+            {renderDot('#8F80F3')}
+            <Text style={{ color: 'white' }}>Okay: 16%</Text>
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              width: 120,
+              marginRight: 20,
+            }}>
+            {renderDot('#3BE9DE')}
+            <Text style={{ color: 'white' }}>Good: 40%</Text>
+          </View>
+          <View
+            style={{ flexDirection: 'row', alignItems: 'center', width: 120 }}>
+            {renderDot('#FF7F97')}
+            <Text style={{ color: 'white' }}>Poor: 3%</Text>
+          </View>
+        </View>
+      </>
+    );
+  };
+
   return (
-    <View className={`flex-1 px-5`} style={{backgroundColor : colors.background}}>
-      <Text className="font-albertMedium">Salam</Text>
+    <View
+      style={{
+        flex: 1,
+      }}>
+      <View
+        style={{
+          margin: 20,
+          padding: 16,
+          borderRadius: 20,
+          backgroundColor: '#202020',
+        }}>
+        <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
+          Performance
+        </Text>
+        <View style={{ padding: 20, alignItems: 'center' }}>
+          <PieChart
+            data={pieData}
+            donut
+            showGradient
+            isAnimated={true}
+            sectionAutoFocus
+            radius={90}
+            innerRadius={60}
+            innerCircleColor={'#232B5D'}
+            centerLabelComponent={() => {
+              return (
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                  <Text
+                    style={{ fontSize: 22, color: 'white', fontWeight: 'bold' }}>
+                    47%
+                  </Text>
+                  <Text style={{ fontSize: 14, color: 'white' }}>Excellent</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+        {renderLegendComponent()}
+      </View>
     </View>
   )
 }
