34d817cd9a47893c7cdf6b00afad20329795d8f5
Faril Mammadov <memmedovfaril@gmail.com>
2025-04-10 01:50:07 +0400

    chore: Icons updated

--- src/navigations/TabNavigation.jsx
+++ src/navigations/TabNavigation.jsx
@@ -6,17 +6,9 @@ import BmiTrackScreen from "@/screens/BmiTrackScreen";
 import WaterTrackScreen from "@/screens/WaterTrackScreen";
 import HealthyFoodsScreen from "@/screens/HealthyFoodsScreen";
 import Fonts from "@/constants/Fonts";
-import HomeActive from "@/assets/icons/HomeActive";
-import HomeInactive from "@/assets/icons/HomeInactive";
-import ProfileActive from "@/assets/icons/ProfileActive";
-import ProfileInactive from "@/assets/icons/ProfileInactive";
-import AppleActive from "@/assets/icons/AppleActive";
-import AppleInactive from "@/assets/icons/AppleInactive";
-import WaterActive from "@/assets/icons/WaterActive";
-import WaterInactive from "@/assets/icons/WaterInactive";
-import BmiActive from "@/assets/icons/BmiActive";
-import BmiInactive from "@/assets/icons/BmiInactive";
 import GoBackButton from "@/components/GoBackButton";
+import { BeefIcon, CircleGaugeIcon, DropletsIcon, HouseIcon, UserRoundIcon } from "lucide-react-native";
+import { Colors } from "@/constants/Colors";
 
 const TabNavigation = () => {
   const { Navigator, Screen, Group } = createBottomTabNavigator();
@@ -29,7 +21,7 @@ const TabNavigation = () => {
           fontFamily: Fonts["600SemiBold"],
           fontSize: 16,
         },
-        headerLeft : () => (<GoBackButton />),
+        headerLeft: () => (<GoBackButton />),
         tabBarStyle: {
           marginBottom: bottom,
           marginHorizontal: 20,
@@ -56,7 +48,7 @@ const TabNavigation = () => {
     >
       <Screen name="Home"
         options={{
-          tabBarIcon: ({ focused }) => focused ? (<HomeActive />) : (<HomeInactive />)
+          tabBarIcon: ({ focused }) => focused ? (<HouseIcon size={24} strokeWidth={2} color={Colors.mainColor} />) : (<HouseIcon size={24} strokeWidth={1.5} color={Colors["light-gray"]} />)
         }}
         component={HomeScreen} />
 
@@ -64,30 +56,30 @@ const TabNavigation = () => {
         name="Water"
         component={WaterTrackScreen}
         options={{
-          headerTitle : "Water Tracker",
-          tabBarIcon : ({focused}) => focused ? (<WaterActive />) : (<WaterInactive />)
+          headerTitle: "Water Tracker",
+          tabBarIcon: ({ focused }) => focused ? (<DropletsIcon size={24} color={Colors.mainColor} strokeWidth={2} />) : (<DropletsIcon size={24} color={Colors["light-gray"]} strokeWidth={1.5} />)
         }}
       />
       <Screen
         name="Bmi"
         component={BmiTrackScreen}
         options={{
-          headerTitle : "Calculate BMI",
-          tabBarIcon : ({focused}) => focused ? (<BmiActive />) : (<BmiInactive />)
+          headerTitle: "Calculate BMI",
+          tabBarIcon: ({ focused }) => focused ? (<CircleGaugeIcon color={Colors.mainColor} size={24} strokeWidth={2} />) : (<CircleGaugeIcon size={24} color={Colors["light-gray"]} strokeWidth={1.5} />)
         }}
       />
-      <Screen 
-      name="Foods" 
-      component={HealthyFoodsScreen}
-      options={{
-        tabBarIcon : ({focused}) => focused ? (<AppleActive />) : (<AppleInactive />)
-      }}
+      <Screen
+        name="Foods"
+        component={HealthyFoodsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => focused ? (<BeefIcon size={24} color={Colors.mainColor} strokeWidth={2} />) : (<BeefIcon size={24} color={Colors["light-gray"]} strokeWidth={1.5} />)
+        }}
       />
       <Screen
         name="Profile"
         component={Screen}
         options={{
-          tabBarIcon: ({ focused }) => focused ? (<ProfileActive />) : (<ProfileInactive />)
+          tabBarIcon: ({ focused }) => focused ? (<UserRoundIcon size={24} color={Colors.mainColor} strokeWidth={2} />) : (<UserRoundIcon size={24} color={Colors["light-gray"]} strokeWidth={1.5} />)
         }}
       />
     </Navigator>
